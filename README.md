03d0055dc955617ffb1f22f7cfcc575cedcaa142
Alex Kleb <akleb@umich.edu>
2023-07-11 16:11:07 -0400

    Dot product test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Linear Algebra Cookbook
-=======================
+The Linear Algebra Cookbook
+===========================
 [![Ubuntu](https://github.com/akleb/LACookbook/actions/workflows/ubuntu.yaml/badge.svg)](https://github.com/akleb/LACookbook/actions/workflows/ubuntu.yaml)
 [![MacOS](https://github.com/akleb/LACookbook/actions/workflows/macos.yaml/badge.svg)](https://github.com/akleb/LACookbook/actions/workflows/macos.yaml)
 [![Memcheck](https://github.com/akleb/LACookbook/actions/workflows/memcheck.yaml/badge.svg)](https://github.com/akleb/LACookbook/actions/workflows/memcheck.yaml)
---
+++ test/test_DotProduct.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file test_DotProduct.cpp
+ * @author Alex Kleb (akleb@umich.edu)
+ * @brief test the Dot product function
+ * @version 0.1
+ * @date 2023-07-11
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "unit_test_framework.h"
+#include "lac_MatrixMath.hpp"
+#include <cstring>
+
+TEST(test_zero){
+
+  const int N = 100;
+  double *a = new double[N];
+  double *b = new double[N];
+
+  srand(0);
+  for (int ii = 0; ii < N; ++ii)
+    a[ii] = (double)rand() / RAND_MAX;
+  std::memset(b, 0, sizeof(double)*N);
+
+  double dot;
+
+  lac_DotProduct(a, b, N, &dot);
+  ASSERT_ALMOST_EQUAL(dot, 0, 1e-12);
+
+  lac_DotProduct(b, a, N, &dot);
+  ASSERT_ALMOST_EQUAL(dot, 0, 1e-12);
+
+  delete[] a;
+  delete[] b;
+  return;
+
+} // test_zero
+
+TEST(test_transpose){
+
+  const int N = 100;
+  double *a = new double[N];
+  double *b = new double[N];
+
+  srand(0);
+  for (int ii = 0; ii < N; ++ii){
+    a[ii] = (double)rand() / RAND_MAX;
+    b[ii] = (double)rand() / RAND_MAX;
+  } // for
+
+  double dot, dot_T;
+
+  lac_DotProduct(a, b, N, &dot);
+  lac_DotProduct(b, a, N, &dot_T);
+
+  ASSERT_ALMOST_EQUAL(dot, dot_T, 1e-12);
+
+  return;
+
+} // test_transpose
+
+TEST(test_basic){
+  
+  double a[4] = {-1, 2, 0, 3};
+  double b[4] = {0.5, 1, 0.3, -0.1};
+
+  double dot;
+  lac_DotProduct(a, b, 4, &dot);
+  ASSERT_ALMOST_EQUAL(dot, 1.2, 1e-12);
+
+  return;
+
+} // test_basic
+
+TEST_MAIN()
+
